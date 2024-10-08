96431f065e2c8b9fa5265b673cdec0a238eca572
ZakiyaY <157597271+ZakiyaY@users.noreply.github.com>
2024-10-08 16:18:41 +0100

    Update client.go

--- client/client.go
+++ client/client.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"flag"
-	"net"
 	"fmt"
+	"net"
+	"os"
 )
 
-func read(conn net.Conn) {
+func read(conn *net.Conn) {
 	//TODO In a continuous loop, read a message from the server and display it.
+	reader := bufio.NewReader(*conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from the server:", err)
+			return
+		}
+		fmt.Println("Message from the server: ", msg)
+	}
 }
 
-func write(conn net.Conn) {
+func write(conn *net.Conn) {
 	//TODO Continually get input from the user and send messages to the server.
+	stdin := bufio.NewReader(os.Stdin) //
+	for {
+		fmt.Printf("Enter text ->")         //User input prompt
+		text, err := stdin.ReadString('\n') //Read user input
+		if err != nil {
+			fmt.Println("Error reading from the server:", err)
+			return
+		}
+		_, err = fmt.Fprintln(*conn, text) //Send input (text) to server
+		if err != nil {
+			fmt.Println("Error sending to the server: ", err)
+			return
+		}
+
+	}
 }
 
 func main() {
@@ -19,6 +45,16 @@ func main() {
 	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
 	flag.Parse()
 	//TODO Try to connect to the server
+	conn, err := net.Dial("tcp", *addrPtr)
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close() //closing the connection when done
+	fmt.Println("Connected to the server at", *addrPtr)
 	//TODO Start asynchronously reading and displaying messages
+	go read(&conn)
+
 	//TODO Start getting and sending user messages.
+	write(&conn)
 }
