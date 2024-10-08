ab540dca0e16466a145f9c72d4a37fef2e6f1a50
ZakiyaY <157597271+ZakiyaY@users.noreply.github.com>
2024-10-08 16:19:22 +0100

    Update server.go

--- server/server.go
+++ server/server.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"flag"
-	"net"
 	"fmt"
+	"net"
 )
 
 type Message struct {
@@ -15,12 +15,21 @@ type Message struct {
 func handleError(err error) {
 	// TODO: all
 	// Deal with an error event.
+	if err != nil {
+		fmt.Println("Error")
+	}
 }
 
 func acceptConns(ln net.Listener, conns chan net.Conn) {
 	// TODO: all
 	// Continuously accept a network connection from the Listener
 	// and add it to the channel for handling connections.
+	for {
+		conn, err := ln.Accept()
+		handleError(err)
+		conns <- conn
+	}
+
 }
 
 func handleClient(client net.Conn, clientid int, msgs chan Message) {
@@ -29,6 +38,18 @@ func handleClient(client net.Conn, clientid int, msgs chan Message) {
 	// Read in new messages as delimited by '\n's
 	// Tidy up each message and add it to the messages channel,
 	// recording which client it came from.
+	reader := bufio.NewReader(client)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		message := Message{clientid, msg}
+		msgs <- message
+
+		fmt.Println(msg)
+		fmt.Fprintln(client, "OK")
+	}
 }
 
 func main() {
@@ -38,6 +59,8 @@ func main() {
 	flag.Parse()
 
 	//TODO Create a Listener for TCP connections on the port given above.
+	ln, err := net.Listen("tcp", *portPtr)
+	handleError(err)
 
 	//Create a channel for connections
 	conns := make(chan net.Conn)
@@ -45,6 +68,8 @@ func main() {
 	msgs := make(chan Message)
 	//Create a mapping of IDs to connections
 	clients := make(map[int]net.Conn)
+
+	clientIDCounter := 0
 
 	//Start accepting connections
 	go acceptConns(ln, conns)
@@ -55,9 +80,21 @@ func main() {
 			// - assign a client ID
 			// - add the client to the clients map
 			// - start to asynchronously handle messages from this client
+			clientId := conn
+			clients[clientIDCounter] = conn
+			fmt.Printf("Client %d connected\n", clientId)
+			go handleClient(conn, clientIDCounter, msgs)
+			clientIDCounter++
+
 		case msg := <-msgs:
+			for id, clientId := range clients {
+				if msg.sender != id {
+					fmt.Fprintf(clientId, msg.message)
+				}
+			}
 			//TODO Deal with a new message
 			// Send the message to all clients that aren't the sender
 		}
 	}
 }
+
